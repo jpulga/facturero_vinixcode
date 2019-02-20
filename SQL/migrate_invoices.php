97a1824cd9ed370dc8f0d57e8ddc6c76b05a42d1
Juan Esteban Garcia <ctxuser@gmail.com>
2019-02-20 14:33:13 -0500

    Script para migrar facturas

--- SQL/migrate_invoices.php
+++ SQL/migrate_invoices.php
@@ -2,8 +2,8 @@
 $usuario = "root";
 $contrasena = "root";  
 $servidor = "localhost";
-$basededatos_vieja = "facturero_vinixcode_old";
-$basededatos_nueva = "facturero_vinixcode";
+$basededatos_vieja = "facturero_vinix";
+$basededatos_nueva = "facturero_v2";
 
 $conexion = mysqli_connect( $servidor, $usuario, $contrasena ) or die ("No se ha podido conectar al servidor de Base de datos");
 $db_vieja = mysqli_select_db( $conexion, $basededatos_vieja ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
@@ -25,15 +25,15 @@ foreach ($facturas as $factura) {
 	$client = $factura['destinatario'];
 	$date = $factura['fecha'];
 	$expiration_date = $factura['fecha_vencimiento'];
-	$document_type = $factura['$tipo_documento'];
+	$document_type = $factura['tipo_documento'];
 	$document_number = $factura['numero_documento'];
     $cellphone = $factura['telefono	'];
     $address = $factura['direccion'];
     $currency = $factura['moneda'];
-    $state = $recibo['estado'];
-    $notes = $recibo['notas'];
+    $state = $factura['estado'];
+    $notes = $factura['notas'];
     $sub_total = str_replace(',', '', $factura['total']);
-    $discount = $recibo['descuento'];
+    $discount = $factura['descuento'];
     $grand_total = str_replace(',', '', $factura['total_pagar']);
 	$created_at = date('Y-m-d H:i:s');
     $updated_at = date('Y-m-d H:i:s');
@@ -43,9 +43,19 @@ foreach ($facturas as $factura) {
         VALUES ($invoice_number, '$company_origin', '$client', '$date', '$expiration_date', '$document_type', '$document_number', '$cellphone', '$address', '$currency', '$state', '$notes', '$sub_total', '$discount', '$grand_total', '$created_at', '$updated_at');   
     ";
     mysqli_query( $conexion, $sql );
-    echo "La factura #" . $invoice_number . " migrado exitosamente.<br />";
-}
 
-foreach ($facturas[productos] as $producto) {
-    # code...
+    $productos = unserialize($factura['productos']);
+    foreach ($productos as $producto) {
+        $name = $producto['descripcion'];
+        $qty = $producto['cantidad'];
+        $price = $producto['valor'];
+        $total = $qty * $price;
+        $sql_products = "
+            INSERT INTO invoice_products (invoice_id, name, qty, price, total, created_at, updated_at) 
+            VALUES ('$invoice_number', '$name', '$qty', '$price', '$total', '$created_at', '$updated_at')";
+        
+        mysqli_query( $conexion, $sql_products );
+    }
+
+    echo "La factura #" . $invoice_number . " migrado exitosamente.<br />";
 }
